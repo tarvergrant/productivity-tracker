3ecf30f7a4d21f0b418a66d8e591faea86089dbb
tarvergrant <tarvergrant@gmail.com>
2024-12-19 11:09:29 -0600

    removed tailwind colors, login UI added

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 function Login() {
-  function login(formData) {
-    const email = formData.get("email");
-    const password = formData.get("password");
-    alert(`Attempted login with email: '${email}' and password:'${password}'.`);
-  }
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    console.log("Username: ", username);
+    console.log("Password:", password);
+  };
   return (
     <div>
       <form
-        action={login}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleLogin}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 border border-black"
       >
         <div className="mb-4">
           <h1 className="text-lg font-bold text-center">Login</h1>
@@ -25,6 +30,7 @@ function Login() {
             id="username"
             type="text"
             placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div className="mb-6">
@@ -35,21 +41,22 @@ function Login() {
             Password
           </label>
           <input
-            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="******************"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="flex items-center justify-between">
           <button
-            className="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="border-2 border-black bg-black text-white bg-oxford_blue-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
           >
             Sign In
           </button>
           <button
-            className="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="border-2 border-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
           >
             Register
