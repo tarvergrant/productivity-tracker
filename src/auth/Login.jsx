1bdd7b9b6fb1aed9ff05aa65661855abae207e70
tarvergrant <tarvergrant@gmail.com>
2024-12-19 10:48:54 -0600

    created basic login form UI

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,5 +1,63 @@
 function Login() {
-  return <></>;
+  function login(formData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
+    alert(`Attempted login with email: '${email}' and password:'${password}'.`);
+  }
+  return (
+    <div>
+      <form
+        action={login}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <h1 className="text-lg font-bold text-center">Login</h1>
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+          <button
+            className="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
