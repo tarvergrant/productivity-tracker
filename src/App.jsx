fada4be6f82f2002032638698fd0968bc2c73502
tarvergrant <tarvergrant@gmail.com>
2024-12-18 21:05:45 -0600

    added style for app wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen flex justify-center">
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
