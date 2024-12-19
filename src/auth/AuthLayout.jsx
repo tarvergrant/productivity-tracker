a82366cfea8d1c855b29cee1b90f74e23c3eb758
tarvergrant <tarvergrant@gmail.com>
2024-12-19 08:35:54 -0600

    populated login elements, moved to auth folder with a wrapper for login and register

---
+++ src/auth/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+
+function AuthLayout() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
+
+export default AuthLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,26 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import Login from "./login/Login.jsx";
-import Register from "./register/Register.jsx";
+import AuthLayout from "./auth/AuthLayout.jsx";
+import Login from "./auth/Login.jsx";
+import Register from "./auth/Register.jsx";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      {/* Public Routes */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route element={<App />}>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+        {/* Protected Routes */}
 
-      {/* Protected Routes */}
-      <Route path="/dashboard" element={<App />} />
-
-      {/* Catch-All Route */}
-      <Route path="*" element={<Navigate to="/login" />} />
+        {/* Catch-All Route */}
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
